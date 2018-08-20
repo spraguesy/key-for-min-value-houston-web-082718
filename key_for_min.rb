bca1f069224e374d6de28ea083c27daf02d8f2dd
Andrew Sprague <andrewsprague@outlook.com>
2018-08-20 19:47:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest_value = name_hash.fetch(name_hash.first)
-  smallest_key = name_hash.first
+  smallest_key = name_hash.key(smallest_value)
   name_hash.each do |key, value|
     if value < smallest_value
       smallest_value = value
